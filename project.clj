56a5df4fb2637f664c3c5be640bbd50f6a8ec4f7
Ivan Grishaev <ivan@grishaev.me>
2023-03-08 11:33:42 +0300

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
 (defproject user-agent "0.1.0"
 
-  :description "User-Agent parser"
-  :url "https://github.com/igrishaev/user-agent"
+  :description
+  "User-Agent parser"
+
+  :url
+  "https://github.com/igrishaev/user-agent"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies
-  [[net.sf.uadetector/uadetector-core "0.9.10"]
+  [[net.sf.uadetector/uadetector-core "0.9.22"]
    [net.sf.uadetector/uadetector-resources "2014.10"]]
 
   :profiles
